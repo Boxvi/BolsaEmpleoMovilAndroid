177cfe554c8df6ef789ce33ac61270dd51cda1e4
Boxvi <hermosokawai@gmail.com>
2022-11-02 08:50:33 -0500

    first commit

---
+++ app/src/main/java/ec/edu/insta/movilgc1/MainActivity.java
@@ -0,0 +1,13 @@
+package ec.edu.insta.movilgc1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
