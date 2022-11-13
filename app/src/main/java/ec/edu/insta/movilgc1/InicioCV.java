e681a4b3a01bb3471ce3f994ec7b0a6166b34c4f
Boxvi <hermosokawai@gmail.com>
2022-11-13 00:20:36 -0500

    Creacion de ventana de incio del cv, y la creacion de la ventana de admin busqueda respectivamente

---
+++ app/src/main/java/ec/edu/insta/movilgc1/InicioCV.java
@@ -0,0 +1,13 @@
+package ec.edu.insta.movilgc1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioCV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_cv);
+    }
+}
